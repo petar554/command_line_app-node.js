8cc378746d349502b36b3a0ace34c7255e358b96
petarPAN <petarburic554@gmail.com>
2021-03-07 19:00:03 +0100

    Refactoring

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ fs.readdir(process.cwd(), (err, filenames) => {
     }
 
 
+    const allStats = Array(filenames.length).fill(null); //[null], [null], [null], [null]
+
     for (let filename of filenames) {
+        // use line below to determine on which index is a filename
+        const index = filenames.indexOf(filename);
+
         fs.lstat(filename, (err, stats) => {
             if (err) {
                 console.log(err);
             }
-            console.log(filename, stats.isFile());
+
+            //console.log(filename, stats.isFile());
+
+            allStats[index] = stats;
+            // check if the every element in the array has a value
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(filenames[index], stats.isFile());
+                });
+            }
         });
     }
 });

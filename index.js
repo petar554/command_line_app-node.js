10167e0fc504b7d51a086423a55019abcac414f9
petarPAN <petarburic554@gmail.com>
2021-03-14 15:05:00 +0100

    Refactoring with promises

--- index.js
+++ index.js
@@ -8,32 +8,17 @@ fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.log(err);
     }
+});
 
 
-    const allStats = Array(filenames.length).fill(null); //[null], [null], [null], [null]
-
-    for (let filename of filenames) {
-        // use line below to determine on which index is a filename
-        const index = filenames.indexOf(filename);
-
+// method 1
+/* const lstat = filename => {
+    return new Promise((resolve, reject) => {
         fs.lstat(filename, (err, stats) => {
             if (err) {
-                console.log(err);
+                reject(err);
             }
-
-            //console.log(filename, stats.isFile());
-
-            allStats[index] = stats;
-            // check if the every element in the array has a value
-            const ready = allStats.every((stats) => {
-                return stats;
-            });
-
-            if (ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile());
-                });
-            }
+            resolve(stats)
         });
-    }
-});
+    });
+} */
